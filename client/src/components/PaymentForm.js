e0869b32f1c295137d1b08260bdf0a351014fc98
Ross White <rosswhite@outlook.com>
2021-07-28 14:51:20 +0100

    Fininshed payment form

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -2,6 +2,23 @@ import React, { useState } from 'react';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import axios from 'axios';
 
+const CARD_OPTIONS = {
+    iconStyle: 'solid',
+    style: {
+        iconColor: '#c4f0ff',
+        color: '#fff',
+        fontweight: 500,
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans=serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': { color: '#fce883' },
+        '::placeholder': { color: '#87bbfd' }
+    },
+    invalid: {
+        iconColor: '#ffc7ee',
+        color: '#ffc7ee'
+    }
+}
 
 export default function PaymentForm() {
     const [success, setSuccess] = useState(false);
@@ -36,8 +53,21 @@ export default function PaymentForm() {
 
 
     return (
+        <>
+        {!success ?
+        <form onSubmit={handleSubmit}>
+            <fieldset className='FormGroup'>
+                <div className='FormRow'>
+                    <CardElement options={CARD_OPTIONS} />
+                </div>
+            </fieldset>
+            <button>Pay</button>
+        </form>
+        :
         <div>
-
+            <h2>Thank you for your payment</h2>
         </div>
+        }
+        </>
     )
 }
