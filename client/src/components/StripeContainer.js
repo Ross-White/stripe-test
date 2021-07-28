eb864e6acc1cb2c9d4b8a4ae980505ae7d52d99d
Ross White <rosswhite@outlook.com>
2021-07-28 14:37:51 +0100

    first commit

---
+++ client/src/components/StripeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const PUBLIC_KEY = "pk_test_51JIBwAB1wXXdtPu0AawJONWu7mSz3W6pNFEjn1PzcdqUYVNi1foru7ZC72XHzymRQHT9F5hk9fwwvED1d9f1olnm008j5uPzFY"
+
+const stripeTestPromise = load(PUBLIC_KEY)
+export default function StripeContainer() {
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm />
+        </Elements>
+    )
+}
